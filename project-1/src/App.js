81b464d154f2e7abd538edcd90313dbfaec9ca96
p4kz <wolpak@hotmail.com>
2022-04-02 11:01:26 -0300

    state example

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          by: p4kz
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: 'Eric',
+    count: 0
+  }
+  
+  handlePClick = () => {
+    this.setState ({name: 'p4kz'})
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault()
+    const { count } = this.state
+    this.setState ({count: count + 1})
+  }
+
+  render() {
+    const { name, count } = this.state
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick}>
+            Hello React {name} {count}
+          </p>
+          <a
+            onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            by: p4kz
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
