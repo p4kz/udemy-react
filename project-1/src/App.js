4daf2c40fd793b7fae33ed18c9c06e2b58dd75fc
p4kz <wolpak@hotmail.com>
2022-04-03 15:19:30 -0300

    lifecycle methods

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -3,7 +3,8 @@ import { Component } from 'react';
 
 class App extends Component {
   state = {
-    post: [ // <- hard coding 
+    counter: 0,
+    post: [ 
       {
         id: 1,
         title: 'o titulo 1',
@@ -23,19 +24,50 @@ class App extends Component {
       }
     ]
   }
+
+  timeoutUpdade = null
+
+  //lifecycle methods
+  componentDidMount() { // <- vai ser executado uma vez assim que o componente for carregado na tela
+    this.handleTimeout()
+  }
+
+  //lifecycle methods
+  componentDidUpdate() { // <- 
+    this.handleTimeout()
+  }
+
+  //lifecycle methods
+  componentWillUnmount() {
+    clearTimeout(this.timeoutUpdade)
+  }
+
+  handleTimeout = () => {
+    const { post, counter } = this.state
+    post[0].title = 'O titulo mudou'
+    post[1].title = 'O titulo mudou aqui'
+    post[2].title = 'O titulo mudou dnv'
+
+    this.timeoutUpdade = setTimeout(() => {
+      this.setState({ post, counter: counter + 1 })
+    }, 1000)
+  }
   
   render() {
-    const { post } = this.state
+    const { post, counter } = this.state
 
     return (
-      <div className="App">        
+
+      <div className="App"> 
+      <h1>{counter}</h1>       
         {post.map(post =>(
-          <div key={post.id}>
+          <div key={post.id}> 
           <h1>{post.title}</h1> 
           <p>{post.body}</p>
           </div>
         ))}       
       </div>
+
     );
   }
 }
