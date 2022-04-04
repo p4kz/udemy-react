d8e8b02c38e6a38414e19c3216fbec456667e349
p4kz <wolpak@hotmail.com>
2022-04-04 14:37:21 -0300

    consumindo api && add dados no front

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,74 +1,53 @@
-import './App.css';
 import { Component } from 'react';
+import './App.css';
 
 class App extends Component {
+  
   state = {
-    counter: 0,
-    post: [ 
-      {
-        id: 1,
-        title: 'o titulo 1',
-        body: 'o corpo',
-      },
-      
-      {
-        id: 2,
-        title: 'o titulo 2',
-        body: 'o corpo 2',
-      },
-
-      {
-        id: 3,
-        title: 'o titulo 3',
-        body: 'o corpo 3',
-      }
-    ]
+    posts: []
   }
 
-  timeoutUpdade = null
-
-  //lifecycle methods
-  componentDidMount() { // <- vai ser executado uma vez assim que o componente for carregado na tela
-    this.handleTimeout()
-  }
-
-  //lifecycle methods
-  componentDidUpdate() { // <- 
-    this.handleTimeout()
-  }
-
-  //lifecycle methods
-  componentWillUnmount() {
-    clearTimeout(this.timeoutUpdade)
-  }
-
-  handleTimeout = () => {
-    const { post, counter } = this.state
-    post[0].title = 'O titulo mudou'
-    post[1].title = 'O titulo mudou aqui'
-    post[2].title = 'O titulo mudou dnv'
-
-    this.timeoutUpdade = setTimeout(() => {
-      this.setState({ post, counter: counter + 1 })
-    }, 1000)
+  // puxando dados da api
+  componentDidMount() { 
+    this.loadPosts()
   }
   
+  loadPosts = async () => {
+    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts/')
+    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
+
+    const [posts, photos] = await Promise.all([ postsResponse, photosResponse ])
+    
+    // convertendo pra json
+    const postsJson = await posts.json()
+    const photosJson = await photos.json()
+
+    // zipper 
+    const postAndPhotos = postsJson.map((post, index) => {
+      return { ...post, cover: photosJson[index].url }
+    })
+
+    this.setState({ posts: postAndPhotos })
+  }
+
   render() {
-    const { post, counter } = this.state
+    const { posts } = this.state
 
     return (
-
-      <div className="App"> 
-      <h1>{counter}</h1>       
-        {post.map(post =>(
-          <div key={post.id}> 
-          <h1>{post.title}</h1> 
-          <p>{post.body}</p>
+      <section className='container'>
+        <div className="posts">
+        {posts.map(post => (
+          <div className='post'>
+            <img src ={post.cover} alt={post.title} />
+            <div key={post.id} className='post-content'> 
+              <h1>{post.title}</h1> 
+              <p>{post.body}</p>
+            </div>
           </div>
         ))}       
-      </div>
-
-    );
+        </div>
+      </section>      
+    )
   }
 }
 
