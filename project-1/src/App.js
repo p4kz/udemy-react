1f6603fcf86494ee94765a3136ec9a6dd4a1bf56
p4kz <wolpak@hotmail.com>
2022-04-05 14:13:26 -0300

    criando components/clean app.js

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -1,32 +1,21 @@
 import { Component } from 'react';
 import './App.css';
 
+import { Posts } from './components/Posts'
+import { loadPosts } from './utils/load-posts'
+
 class App extends Component {
   
   state = {
     posts: []
   }
 
-  // puxando dados da api
   componentDidMount() { 
     this.loadPosts()
   }
   
   loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts/')
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
-
-    const [posts, photos] = await Promise.all([ postsResponse, photosResponse ])
-    
-    // convertendo pra json
-    const postsJson = await posts.json()
-    const photosJson = await photos.json()
-
-    // zipper 
-    const postAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url }
-    })
-
+    const postAndPhotos = await loadPosts()
     this.setState({ posts: postAndPhotos })
   }
 
@@ -35,17 +24,7 @@ class App extends Component {
 
     return (
       <section className='container'>
-        <div className="posts">
-        {posts.map(post => (
-          <div className='post'>
-            <img src ={post.cover} alt={post.title} />
-            <div key={post.id} className='post-content'> 
-              <h1>{post.title}</h1> 
-              <p>{post.body}</p>
-            </div>
-          </div>
-        ))}       
-        </div>
+        <Posts posts={ posts } />
       </section>      
     )
   }
