8fc9ba8ca2fc0e2112d9a7ef175ca50b46f6bacb
p4kz <wolpak@hotmail.com>
2023-01-31 08:25:35 -0300

    fix custom hook 1 - react hooks

--- react-hooks-dom/src/pages/Home/index.jsx
+++ react-hooks-dom/src/pages/Home/index.jsx
@@ -1,47 +1,110 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './styles.css'
+
+const isObjectEqual = (objA, objB) => {
+  return JSON.stringify(objA) === JSON.stringify(objB)
+}
 
 const useFetch = (url, options) => {
   const [result, setResult] = useState(null)
-  const [loading, setLoading] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [shouldLoad, setShouldLoad] = useState(false)
+  const urlRef = useRef(url)
+  const optionsRef = useRef(options)
 
   useEffect(() => {
+    let changed = false
+
+    if(!isObjectEqual(url, urlRef.current)) {
+      urlRef.current = url;
+      changed = true
+    }
+
+    if(!isObjectEqual(options, optionsRef.current)) {
+      optionsRef.current = options;
+      changed = true
+    }
+
+    if(changed) {
+      setShouldLoad(s => !s)
+    }
+
+  },[url, options]) 
+
+  useEffect(() => {
+    let wait = false
+
+    console.log(optionsRef.current.headers)
+
     setLoading(true)
 
     const fetchData = async () => {
-      await new Promise(r => setTimeout(r, 3000))
+      await new Promise((r) => setTimeout(r, 1000))
 
       try {
-        const response = await fetch(url, options)
+        const response = await fetch(urlRef.current, optionsRef.current)
         const jsonResult = await response.json()
-        setResult(jsonResult)
-        setLoading(false)
+
+        if(!wait) {
+          setResult(jsonResult)
+          setLoading(false)
+        }
       } catch(e) {
-        setLoading(false)
+        if(!wait) {
+          setLoading(false)
+        }
         throw e
       }
     }
 
     fetchData()
-  }, [url])
+
+    return () => {
+      wait = true
+    }
+    
+  }, [shouldLoad])
 
   return [result,loading]
 }
 
 export const Home = () => {
-  const [ result, loading ] = useFetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'GET',
+  const [postId, setPostId] = useState('')
+  const [ result, loading ] = useFetch('https://jsonplaceholder.typicode.com/posts/' + postId, {
     headers: {
-      abc: '1',
-    },
+      abc: "1" + postId,
+    }
   })
 
+  useEffect(() => {
+    console.log(postId, "post id")
+    
+  }, [postId])
+  
   if(loading) {
     return <p>Loading...</p>
   }
 
-  if(!loading) {
-    return console.log(result)
+  const handleClick = (id) => {
+    setPostId(id)
+  }
+
+  if(!loading && result) {
+    return (
+      <div>
+        {result?.length > 0 ? (
+          result.map((p) => (
+            <div key={`post-${p.id}`} onClick={() => {handleClick(p.id)}}> 
+              <p>{p.title}</p>
+            </div>
+          )) 
+        ) : (
+          <div onClick={() => handleClick('')}> 
+            <p>{result.title}</p>
+          </div>
+        )}
+      </div> 
+    )
   }
 
   return (
