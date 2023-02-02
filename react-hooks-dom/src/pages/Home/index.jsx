581a23e79b06fa16c1f4878e932f8a4bf5647ccc
p4kz <wolpak@hotmail.com>
2023-02-02 06:07:10 -0300

    create module use fetch - react hooks

--- react-hooks-dom/src/pages/Home/index.jsx
+++ react-hooks-dom/src/pages/Home/index.jsx
@@ -1,75 +1,7 @@
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
+import { useFetch } from './use-fetch'
+
 import './styles.css'
-
-const isObjectEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB)
-}
-
-const useFetch = (url, options) => {
-  const [result, setResult] = useState(null)
-  const [loading, setLoading] = useState(false)
-  const [shouldLoad, setShouldLoad] = useState(false)
-  const urlRef = useRef(url)
-  const optionsRef = useRef(options)
-
-  useEffect(() => {
-    let changed = false
-
-    if(!isObjectEqual(url, urlRef.current)) {
-      urlRef.current = url;
-      changed = true
-    }
-
-    if(!isObjectEqual(options, optionsRef.current)) {
-      optionsRef.current = options;
-      changed = true
-    }
-
-    if(changed) {
-      setShouldLoad(s => !s)
-    }
-
-  },[url, options]) 
-
-  useEffect(() => {
-    let wait = false
-    const controller = new AbortController()
-    const signal = controller.signal
-
-    console.log(optionsRef.current.headers)
-
-    setLoading(true)
-
-    const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 1000))
-
-      try {
-        const response = await fetch(urlRef.current, {signal, ...optionsRef.current})
-        const jsonResult = await response.json()
-
-        if(!wait) {
-          setResult(jsonResult)
-          setLoading(false)
-        }
-      } catch(e) {
-        if(!wait) {
-          setLoading(false)
-        }
-        throw e
-      }
-    }
-
-    fetchData()
-
-    return () => {
-      wait = true
-      controller.abort()
-    }
-    
-  }, [shouldLoad])
-
-  return [result,loading]
-}
 
 export const Home = () => {
   const [postId, setPostId] = useState('')
@@ -78,12 +10,7 @@ export const Home = () => {
       abc: "1" + postId,
     }
   })
-
-  useEffect(() => {
-    console.log(postId, "post id")
-    
-  }, [postId])
-  
+ 
   if(loading) {
     return <p>Loading...</p>
   }
