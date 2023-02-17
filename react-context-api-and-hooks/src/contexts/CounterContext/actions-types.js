7554e665f7539e27532ab78d3bc06b6b051721af
p4kz <wolpak@hotmail.com>
2023-02-17 03:02:31 -0300

    create reducer - react hooks

--- react-context-api-and-hooks/src/contexts/CounterContext/actions-types.js
+++ react-context-api-and-hooks/src/contexts/CounterContext/actions-types.js
@@ -6,3 +6,5 @@ export const DECREASE = `${prefix}/DECREASE`
 export const RESET = `${prefix}/RESET`
 export const SET_COUNTER = `${prefix}/SET_COUNTER`
 export const ASYNC_INCREASE_START = `${prefix}/ASYNC_INCREASE_START`
+export const ASYNC_INCREASE_END = `${prefix}/ASYNC_INCREASE_START`
+export const ASYNC_INCREASE_ERROR = `${prefix}/ASYNC_INCREASE_START`
