ade18de3ceb47d16104bddf1a8bd167f4acc5da6
p4kz <wolpak@hotmail.com>
2023-03-02 06:32:55 -0300

    create routes - react router dom

---
+++ react-router-dom/src/components/About/index.tsx
@@ -0,0 +1,10 @@
+
+import './styles.css'
+
+export const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  )
+} 
--- react-router-dom/src/main.tsx
+++ react-router-dom/src/main.tsx
@@ -3,8 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { Home } from './components/Home'
 import './styles/global.css'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { About } from './components/About'
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/about' element={ <About /> } />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
