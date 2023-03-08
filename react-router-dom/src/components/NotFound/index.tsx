390a2c5c5e4517662321a7ff07ec6da34b3621f1
p4kz <wolpak@hotmail.com>
2023-03-08 06:43:44 -0300

    add not found page - react router dom

---
+++ react-router-dom/src/components/NotFound/index.tsx
@@ -0,0 +1,10 @@
+
+import './styles.css'
+
+export const NotFound = () => {
+  return (
+    <div>
+      <h1>page not found</h1>
+    </div>
+  )
+} 
--- react-router-dom/src/main.tsx
+++ react-router-dom/src/main.tsx
@@ -8,6 +8,7 @@ import { About } from './components/About'
 import { Menu } from './components/Menu'
 import { Post } from './components/Post'
 import { Redirect } from './components/Redirect'
+import { NotFound } from './components/NotFound'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -19,6 +20,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <Route path='/post/:id' element={ <Post /> } />
         <Route path='/posts' element={ <Post /> } />
         <Route path='/redirect' element={ <Redirect /> } />
+        <Route path='/*' element={ <NotFound /> } />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
