40c6d00ea3b920ef7b8414cf71365804ed5bcd38
p4kz <wolpak@hotmail.com>
2023-03-29 10:56:17 -0300

    create heading component - react landing page

--- react-landing-page/src/styles/global-styles.js
+++ react-landing-page/src/styles/global-styles.js
@@ -18,7 +18,7 @@ export const GlobalStyles = createGlobalStyle`
     font-size: 1.6rem;
   }
 
-  h1 {
+  h1, h2, h3, h4, h5, h6 {
     font-family: ${({ theme }) => theme.font.family.secundary};
   }
 `;
--- react-landing-page/src/styles/theme.js
+++ react-landing-page/src/styles/theme.js
@@ -9,6 +9,18 @@ export const theme = {
     family: {
       default: "'Open Sans', sans-serif",
       secundary: "'Montserrat', sans-serif",
+    },
+    
+    sizes: {
+      xsmall: '0.8rem',
+      small: '1.6rem',
+      medium: '2.rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
+  
     }
-  }
+  },
 }
